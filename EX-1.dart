535a93c8bd8e7f3c0d68874aba3fc0e79ba14f33
Botr-88 <virakboth291@gmail.com>
2026-05-18 13:48:23 +0700

    Update EX-1.dart

--- EX-1.dart
+++ EX-1.dart
@@ -1,3 +1,4 @@
+
 enum Skill {
   FLUTTER,
   DART,
@@ -10,7 +11,7 @@ enum Skill {
 class Address {
   String street;
   String city;
-  int zipCode;
+  String zipCode;
   Address(this.street, this.city, this.zipCode);
 }
 
@@ -27,7 +28,7 @@ class Employee {
     this._address,
     this._yearsOfExperience,
   );
-  Employee.mobileDev(String name, double salary, Address address, int years)
+  Employee.mobileDeveloper(String name, double salary, Address address, int years)
     : _name = name,
       _baseSalary = 40000,
       _skills = [Skill.FLUTTER, Skill.DART],
@@ -38,6 +39,19 @@ class Employee {
   List<Skill> get skills => _skills;
   Address get address => _address;
   int get yearsOfExperience => _yearsOfExperience;
+
+  void printDetails() {
+    print('--- Employee Details ---');
+    print('Name: $_name');
+    print('City: ${_address.city}');
+    print('Street: ${_address.street}');
+    print('Zip Code: ${_address.zipCode}');
+    print('Experience: $_yearsOfExperience years');
+    print('Skills: ${_skills.map((s) => s.name).join(', ')}');
+    print('Total Salary: \$${computeSalary()}');
+    print('');
+  }
+
 
   double computeSalary() {
     double total = _baseSalary;
@@ -56,12 +70,30 @@ class Employee {
 }
 
 void main() {
-  Address addr = Address("PL1", "Phnom Penh", 12000);
-  Employee emp = Employee.mobileDev("bot", 9999, addr, 2);
+  // Address addr = Address("PL1", "Phnom Penh", 12000);
+  // Employee emp = Employee.mobileDev("bot", 9999, addr, 2);
 
-  print("Name: ${emp.name}");
-  print("City: ${emp.address.city}");
-  print("Experience: ${emp.yearsOfExperience}years");
-  print("Skills: ${emp.skills.map((s) => s.name).join(', ')}");
-  print("Total Salary:\$${emp.computeSalary()}");
+  // print("Name: ${emp.name}");
+  // print("City: ${emp.address.city}");
+  // print("Experience: ${emp.yearsOfExperience}years");
+  // print("Skills: ${emp.skills.map((s) => s.name).join(', ')}");
+  // print("Total Salary:\$${emp.computeSalary()}");
+   var emp1 = Employee(
+    'Sokea',
+    40000,
+    [Skill.DART, Skill.FLUTTER],
+    Address('Street 1', 'Phnom Penh', '12001'),
+    10,
+  );
+  emp1.printDetails();
+
+  var emp2 = Employee(
+    'Ronan',
+    45000,
+    [Skill.FLUTTER, Skill.OTHER, Skill.DART],
+    Address('Street 6A', 'Phnom Penh', '12000'),
+    10,
+  );
+  emp2.printDetails();
+
 }
